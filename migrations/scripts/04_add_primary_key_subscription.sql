8b6dd2f65c50c870d10795bd9dc09862cbea2db4
Murtaza Burakaev <muoretaza@gmail.com>
2023-04-30 15:46:56 +0300

    the result of task 6.1

--- migrations/scripts/04_add_primary_key_subscription.sql
+++ migrations/scripts/04_add_primary_key_subscription.sql
@@ -1,4 +1,5 @@
 --liquibase formatted sql
 
 --changeset fenolmbyte:CRS-4_add_primary_key_subscription
-ALTER  TABLE  subscription  ADD  PRIMARY  KEY  (chat_id, link_id);
+ALTER TABLE subscription
+    ADD PRIMARY KEY (chat_id, link_id);
