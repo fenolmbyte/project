31e57f70472742665b912c708f4790462797c74e
Murtaza Burakaev <muoretaza@gmail.com>
2023-05-18 22:06:36 +0300

    added some changes to the scrapper service files

--- scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/service/domain/api/LinkService.java
+++ scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/service/domain/api/LinkService.java
@@ -1,10 +1,9 @@
 package ru.tinkoff.edu.java.scrapper.service.domain.api;
 
-import ru.tinkoff.edu.java.scrapper.dto.model.Link;
-
 import java.time.Duration;
 import java.time.OffsetDateTime;
 import java.util.List;
+import ru.tinkoff.edu.java.scrapper.dto.model.Link;
 
 public interface LinkService {
     List<Link> updateLastCheckedTimeAndGet(Duration linkToBeCheckedInterval);
